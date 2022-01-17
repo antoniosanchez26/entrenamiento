b4bbe09a9d27515a1b2959e148a1a01f1ab42fc5
al036991 <al036991@ieslaencanta.com>
2022-01-17 10:39:32 +0100

    Inicio de For2

---
+++ For2/src/es/asdj/Main.java
@@ -0,0 +1,16 @@
+package es.asdj;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int suma = 0;
+        int n;
+
+        System.out.println(  )
+
+        for (int i = 0; i < n ; i++) {
+            suma = suma + i + 1 ;
+        }
+        System.out.println( "Suma = " + suma);
+    }
+}
