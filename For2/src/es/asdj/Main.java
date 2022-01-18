f56082aeb25bc7ab07592248a20bb027e15cbe3a
Antonio Sánchez <al036991@ieslaencanta.com>
2022-01-18 12:29:33 +0100

    for 2 terminado

--- For2/src/es/asdj/Main.java
+++ For2/src/es/asdj/Main.java
@@ -1,15 +1,19 @@
 package es.asdj;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         int suma = 0;
         int n;
 
-        System.out.println(  )
+        Scanner entrada = new Scanner(System.in);
+        System.out.print( "Dame un número: ");
+        n = entrada.nextInt();
 
         for (int i = 0; i < n ; i++) {
-            suma = suma + i + 1 ;
+            suma += i + 1 ;
         }
         System.out.println( "Suma = " + suma);
     }
