21ca5725b9617555c545cbe854f9e879ec4120de
Antonio Sánchez <al036991@ieslaencanta.com>
2022-02-22 12:41:08 +0100

    arrays2 empezado

--- Arrays1/src/es/asdj/Main.java
+++ Arrays1/src/es/asdj/Main.java
@@ -8,18 +8,18 @@ public class Main {
         int[] numeros = new int[10];
         Scanner entrada = new Scanner(System.in);
 
-        for (int i = 0 ; i < numeros.length; i ++) {
-            System.out.print("Número "+ (i + 1 )+ ": ");
+        for (int i = 0 ; i < numeros.length; i++) {
+            System.out.print("Número " + (i + 1) + ": ");
             numeros[i] = entrada.nextInt();
         }
         System.out.println("Estos son los números que me has dado: ");
-        for (int i = 0 ; i < numeros.length; i ++) {
-            if (i < numeros.length - 1){
+        for (int i = 0 ; i < numeros.length; i++) {
+            if (i < numeros.length - 2) {
                 System.out.print(numeros[i] + ", " );
             } else if (i == numeros.length - 1) {
-                System.out.print("y " + numeros[i] );
+                System.out.print(" y " + numeros[i]);
             } else {
-                System.out.print(numeros[i] );
+                System.out.print(numeros[i]);
             }
         }
         entrada.close();
---
+++ Arrays2/src/es/asdj/Main.java
@@ -0,0 +1,35 @@
+package es.asdj;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] numeros = new int[10];
+        int numero,suma = 0;
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("cargando números...");
+        System.out.println();
+
+        for (int i = 0 ; i < numeros.length; i++) {
+          numeros[i] = (int)(Math.random() *10) + 1;
+        }
+        System.out.print("Dame un numero: ");
+        numero = entrada.nextInt();
+
+        for (int i = 0 ; i < numeros.length; i++) {
+            if (numero == numeros[i]) {
+                suma = suma + 1;
+            }
+        }
+        System.out.println();
+        System.out.println("El número " + numero + " está " + suma + " veces");
+        System.out.println();
+
+        for (int i = 0 ; i < numeros.length; i++) {
+            System.out.print(numeros[i] + " ");
+        }
+        entrada.close();
+    }
+}
