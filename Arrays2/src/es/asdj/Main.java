473a5b0b5b1916341aa14df8f7b989ec8fce95a5
Antonio Sánchez <al036991@ieslaencanta.com>
2022-02-22 12:51:02 +0100

    arrays2 con espacios de román

--- Arrays2/src/es/asdj/Main.java
+++ Arrays2/src/es/asdj/Main.java
@@ -6,30 +6,31 @@ public class Main {
 
     public static void main(String[] args) {
         int[] numeros = new int[10];
-        int numero,suma = 0;
+        int numero, contador = 0;
         Scanner entrada = new Scanner(System.in);
 
         System.out.println("cargando números...");
         System.out.println();
-
         for (int i = 0 ; i < numeros.length; i++) {
-          numeros[i] = (int)(Math.random() *10) + 1;
+          numeros[i] = (int)(Math.random() * 10) + 1;
         }
+
         System.out.print("Dame un numero: ");
         numero = entrada.nextInt();
 
         for (int i = 0 ; i < numeros.length; i++) {
             if (numero == numeros[i]) {
-                suma = suma + 1;
+                contador++;
             }
         }
-        System.out.println();
-        System.out.println("El número " + numero + " está " + suma + " veces");
-        System.out.println();
 
+        System.out.println();
+        System.out.println("El número " + numero + " está " + contador + " veces");
+        System.out.println();
         for (int i = 0 ; i < numeros.length; i++) {
             System.out.print(numeros[i] + " ");
         }
+
         entrada.close();
     }
 }
