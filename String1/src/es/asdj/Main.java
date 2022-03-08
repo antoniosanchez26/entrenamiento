6c608e98f7781800050663437df31a368552839f
Antonio Sánchez <al036991@ieslaencanta.com>
2022-03-08 12:55:18 +0100

    string1 iniciado

---
+++ String1/src/es/asdj/Main.java
@@ -0,0 +1,21 @@
+package es.asdj;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        String l, letra;
+
+        l = entrada.nextLine();
+        System.out.print("Intenta adivinar una letra del texto: ");
+        letra = entrada.nextLine();
+
+        for (int i = 0; i < l.length ; i++) {
+            if (letra = l.charAt(i)) {
+
+            }
+        }
+    }
+}
