bd5b88eb9a4b8c95e22ef4dd91c2b7046741b884
al036991 <al036991@ieslaencanta.com>
2022-03-14 10:39:57 +0100

    string 1

--- String1/src/es/asdj/Main.java
+++ String1/src/es/asdj/Main.java
@@ -1,5 +1,6 @@
 package es.asdj;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
@@ -14,16 +15,25 @@ public class Main {
         System.out.print("Intenta adivinar una letra del texto: ");
         char letra = entrada.nextLine().charAt(0);
 
-        for (int i = 0; i < texto.length(); i++) {
+        /*for (int i = 0; i < texto.length(); i++) {
             if (letra == texto.charAt(i)) {
                 contador++;
             }
         }
-
         if (contador == 0){
             System.out.println("La letra " + letra + " no está en el texto: " + texto);
         } else {
             System.out.println("La letra " + letra + "  está en el texto: " + texto + " y aparece " + contador + " veces");
+        }*/
+
+        for (int i = 0; i < texto.length(); i++) {
+            if (letra == texto.toLowerCase().charAt(i) || letra == texto.toUpperCase().charAt(i)) {
+                System.out.print(texto.charAt(i));
+            } else if  (texto.charAt(i) == ' '){
+                System.out.print(" ");
+            } else {
+                System.out.print("_");
+            }
         }
     }
 }
